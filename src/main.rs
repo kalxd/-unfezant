b3504e5c7d682e578e12184b3083762f61882f77
荀洪道 <kalxd@126.com>
2024-06-13 20:02:33 +0800

    基本布局。

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use gtk::{
-	prelude::{ApplicationExt, ApplicationExtManual, ContainerExt, WidgetExt},
-	Application, ApplicationWindow, Box as GtkBox,
+	prelude::{ApplicationExt, ApplicationExtManual, BoxExt, ContainerExt, WidgetExt},
+	Application, ApplicationWindow, Box as GtkBox, Button, Entry, Orientation, TextView,
 };
 
 fn build_ui(app: &Application) {
-	let window = ApplicationWindow::builder().application(app).build();
-	let layout = GtkBox::builder().build();
+	let window = ApplicationWindow::builder()
+		.application(app)
+		.width_request(600)
+		.height_request(400)
+		.build();
+	let layout = GtkBox::builder().orientation(Orientation::Vertical).build();
+
+	let text_view = TextView::new();
+	layout.pack_start(&text_view, true, true, 10);
+
+	let send_layout = GtkBox::builder().build();
+	let entry = Entry::new();
+	send_layout.pack_start(&entry, true, true, 0);
+	let btn = Button::builder().label("发送").build();
+	send_layout.pack_start(&btn, false, true, 10);
+	layout.pack_start(&send_layout, false, true, 0);
+
 	window.add(&layout);
 	window.show_all();
 }
