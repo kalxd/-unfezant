6fc411cf181fedbc09d27bb247e51ddc25bec755
荀洪道 <kalxd@126.com>
2024-06-14 20:22:50 +0800

    加点私货。

--- src/client.rs
+++ src/client.rs
@@ -2,21 +2,22 @@ use rumqttc::{Client, MqttOptions, QoS};
 
 const CHANNEL: &str = "hello/world";
 
+pub trait SendMessage {
+	fn send<T: Into<Vec<u8>>>(&self, payload: T);
+}
+
+impl SendMessage for Client {
+	fn send<T: Into<Vec<u8>>>(&self, payload: T) {
+		self.publish(CHANNEL, QoS::AtLeastOnce, false, payload)
+			.unwrap();
+	}
+}
+
 pub fn run_client() {
 	let config = MqttOptions::new("1", "127.0.0.1", 1883);
 	let (client, mut conn) = Client::new(config, 1);
 
 	client.subscribe(CHANNEL, QoS::AtLeastOnce).unwrap();
-
-	/*
-		std::thread::spawn(move || {
-			for _ in 0..10 {
-				client
-					.publish(CHANNEL, QoS::AtLeastOnce, false, [1, 2, 3])
-					.unwrap();
-			}
-	});
-		*/
 
 	for msg in conn.iter() {
 		println!("{:?}", msg);
