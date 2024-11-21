6fb5a1aa0ec9006c00c695d49da0fdc7d3c44db4
荀洪道 <kalxd@126.com>
2024-11-21 19:31:22 +0800

    可以发送了。

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use client::SendMessage;
+use futures::{channel::mpsc, future::ready, StreamExt};
 use gtk4::{
+	glib,
 	prelude::{ApplicationExt, ApplicationExtManual, BoxExt, GtkWindowExt},
 	Application, ApplicationWindow, Box as GtkBox, Orientation,
 };
 use rumqttc::{Event, Packet};
 
+use std::{str::from_utf8, thread};
+
 mod client;
 mod server;
 mod widget;
 
-use core::str;
-use std::thread;
+enum MessageAction {
+	Packet(String),
+}
 
 fn setup_ui(app: &Application) {
-	thread::spawn(|| server::run_server());
+	let (mut sx, rx) = mpsc::channel::<MessageAction>(10);
 
 	let layout = GtkBox::builder()
 		.orientation(Orientation::Vertical)
@@ -37,6 +42,7 @@ fn setup_ui(app: &Application) {
 	window.present();
 
 	let (client, mut conn) = client::new_client();
+
 	thread::spawn({
 		move || {
 			conn.iter()
@@ -50,10 +56,18 @@ fn setup_ui(app: &Application) {
 					_ => None,
 				})
 				.for_each(|x| {
-					let msg = format!("接收到：{:?}", str::from_utf8(&x.payload));
-					dbg!(msg);
+					let msg = format!("接收到：{:?}", from_utf8(&x.payload));
+					sx.try_send(MessageAction::Packet(msg)).unwrap();
 				});
 		}
+	});
+
+	glib::MainContext::default().spawn_local(async move {
+		rx.for_each(|MessageAction::Packet(packet)| {
+			log_view.append_log(&packet);
+			ready(())
+		})
+		.await;
 	});
 
 	messager.connect_send_message(move |msg| {
@@ -62,6 +76,8 @@ fn setup_ui(app: &Application) {
 }
 
 fn main() {
+	thread::spawn(|| server::run_server());
+
 	let app = Application::builder()
 		.application_id("person.xgley.unfezant")
 		.build();
